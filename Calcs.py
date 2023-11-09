6dbe02a5a7aae4d3e856a5d43fccafdfb5f94df7
Rafael Nogueira Serpa <rafaeln.serpa@gmail.com>
2023-11-08 22:35:00 -0300

    Trying something

---
+++ Calcs.py
@@ -0,0 +1,21 @@
+
+
+
+def Calc_NTurns(N_turns1,indutance1,coil_area_e1,coil_len1): 
+    print("Numr de voltas")
+    pass
+
+def Calc_ind(N_turns1,indutance1,coil_area_e1,coil_len1): 
+    print("Indutancia")
+
+    pass 
+
+def Calc_coilA(N_turns1,indutance1,coil_area_e1,coil_len1): 
+    print("Area das espiras")
+
+    pass
+
+def Calc_coil_len(N_turns1,indutance1,coil_area_e1,coil_len1): 
+    print("Comprimento das espiras ")
+
+    pass
