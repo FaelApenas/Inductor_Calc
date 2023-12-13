27e4849aa677e8e6e29f5fdd6a9422db99bd3a12
Rafael Nogueira Serpa <126-rafael.serpa1@users.noreply.192.168.0.185>
2023-12-13 16:07:37 -0300

    implement the type selector

--- Aux_fun.py
+++ Aux_fun.py
@@ -2,9 +2,16 @@ u= 0.000001 # micro
 m= 0.001# mili
 
 def f_calc_type(numbers):
-    if "" in numbers: 
-        print(nu) 
-    pass
+    verify_lock=True
+    Calc_type= 0   #The type of the calc will be defined by this number 
+    for number in numbers: 
+        if number =="":
+            if verify_lock==False: 
+                return  0xFF
+            Calc_type= Calc_type + numbers.index(number)+1
+            verify_lock=False 
+    return Calc_type 
+ 
 def handle_data(data): 
     if(data[-1:]=='u'): 
         data = data.rstrip(data[-1])
--- Calcs.py
+++ Calcs.py
@@ -1,52 +1,45 @@
 import numpy as np
+from Aux_fun import * 
 u= 0.000001 # micro
 m= 0.001# mili
 
-
-
 def calcs_data(numbers): 
-    counter= 0
-    for item in numbers: 
-       if item == "" : 
-        counter= counter +(numbers.index(item)+5)
-        print(counter)
-        
-    if counter>8: 
+    type_calc= f_calc_type(numbers)
+    if type_calc==0xFF: 
         print("Only one variable should be empty") 
         return
     else: 
-        match counter: 
-            case 5: 
-                result= Calc_coilA(numbers) 
+        match type_calc: 
+            case 1: 
+                result= first_var(numbers) 
                 return result
-
-            case 6: 
-                result= Calc_coil_len(numbers)
-                return
-            case 7: 
-                result= Calc_ind(numbers)
-                return
-            case 8: 
-                result= Calc_NTurns(numbers)
-                return
+            case 2: 
+                result= second_var(numbers)
+                return result
+            case 3: 
+                result= third_var(numbers)
+                return result
+            case 4: 
+                result= fourth_var(numbers)
+                return result 
     
 
 
-def Calc_NTurns(numbers): 
+def fourth_var(numbers): 
     print("Numr de voltas")
     pass
 
-def Calc_ind(numbers): 
+def third_var(numbers): 
     print("Indutancia")
 
     pass 
 
-def Calc_coilA(numbers): 
+def first_var(numbers): 
     print("Area das espiras")
 
     pass
 
-def Calc_coil_len(numbers): 
+def second_var(numbers): 
     print("Comprimento das espiras ")
 
     pass
